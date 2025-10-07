2c42ab482668bf9d069d1893d79bcb45e734fef5
Morad Hussein <moradhussein063@gmail.com>
2025-10-07 13:31:02 +0300

    p1

---
+++ GymManagementDAL/Repositories/Interfaces/IGenericRepository.cs
@@ -0,0 +1,27 @@
+﻿using GymManagementDAL.Models.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymManagementDAL.Repositories.Interfaces
+{
+    internal interface IGenericRepository<TEntity> where TEntity : BaseEntity, new()
+    {
+        // GetAll
+        IEnumerable<TEntity> GetAll();
+        //GetByID
+        TEntity? GetById(int id);
+        // ADD 
+        int Add(TEntity entity);
+        // Update
+        int Update (TEntity entity);
+        // Delete 
+        int Delete (TEntity entity);
+
+
+
+
+    }
+}
