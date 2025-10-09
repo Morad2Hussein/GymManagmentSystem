38e2deb5aa2e302fbe8ffd40e163883352a6a4ab
Morad Hussein <moradhussein063@gmail.com>
2025-10-10 02:26:03 +0300

    Trainer [Services ]

---
+++ GymManagementBll/Services/Interfaces/ITranierService.cs
@@ -0,0 +1,18 @@
+﻿
+using GymManagementBll.ViewModels.TrainerModels;
+
+namespace GymManagementBll.Services.Interfaces
+{
+    public interface ITranierService
+    {
+        IEnumerable<TrainerViewModels> GetAllTrinaer();
+        TrainerViewModels? GetTrainerDetailsById(int TrainerId);
+        bool CreateTrainer(TrainerCreateViewModel trainerCreateViewModel);
+        UpdateTrainerViewModel? GetTrainerUpdate(int id);
+        bool UpdateTrainerDetails(   int Id, UpdateTrainerViewModel updateTrainerViewModel);
+
+        bool DeleteTrainerDetails(int Id);
+
+
+    }
+}
---
+++ GymManagementBll/ViewModels/TrainerModels/UpdateTrainerViewModel.cs
@@ -0,0 +1,55 @@
+﻿using GymManagementDAL.Models.Enum;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymManagementBll.ViewModels.TrainerModels
+{
+    public class UpdateTrainerViewModel
+    {
+        public String Name { get; set; } = null!;
+     
+
+
+        #region Email
+        [Required(ErrorMessage = "Email is Required Please Enter Your Email Address.")] // Email is required 
+        [StringLength(100, MinimumLength = 10, ErrorMessage = "Email Must Not Exceed 100 Characters.")] // Max 100 char
+        [EmailAddress(ErrorMessage = "Please Enter A Valid Email Address.")] // Validtion 
+        [DataType(DataType.EmailAddress)] // UI HINT
+        public string Email { get; set; } = null!;
+        #endregion
+
+        #region PhoneNumber 
+        [Required(ErrorMessage = "Phone is Required Please Enter Your Phone.")]
+        [Phone(ErrorMessage = "Invliod Phone Format ")]
+        [RegularExpression(@"^(011|012|015)\d{8}$", ErrorMessage = "Phone Number Must Be Valid Egyption PhoneNumber")]
+        [DataType(DataType.PhoneNumber)] // UI HINT TO MAKE USER KeyBoard AS A Formate Number 
+        public string Phone { get; set; } = null!;
+        #endregion
+
+     #region Specialties
+        [Required(ErrorMessage = "Specialties Is Requried Please Enter Your Specialties")]
+        public Specialties Specialties { get; set; } 
+        #endregion
+        #region Address
+        [Required(ErrorMessage = "Building Number Is Required ")]
+        [Range(1, 900, ErrorMessage = "Building Number be Between 1 to 900")]
+        public int BuildingNumber { get; set; }
+        [Required(ErrorMessage = "Street Is Required ")]
+        [StringLength(150, MinimumLength = 3, ErrorMessage = "You Must Write Your Street Between 3 and  150 Charaters")]
+        [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "Street an Contain Only Letters And Spaces ")]
+        public string Street { get; set; } = null!;
+        [Required(ErrorMessage = "Building Number Is Required ")]
+        [StringLength(150, MinimumLength = 3, ErrorMessage = "You Must Write Your City Between 3 and  150 Charaters")]
+        [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "City an Contain Only Letters And Spaces ")]
+        public string City { get; set; } = null!;
+        #endregion
+
+
+    }
+}
+
+
