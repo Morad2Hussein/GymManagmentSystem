123c4db5dd9816a552cfe70f4ae65e16ae87df5a
Morad Hussein <moradhussein063@gmail.com>
2025-10-09 12:16:19 +0300

    PlanServices

---
+++ GymManagementBll/Services/Classes/PlanServiec.cs
@@ -0,0 +1,130 @@
+﻿using GymManagementBll.Services.Interfaces;
+using GymManagementBll.ViewModels.PlanViewModels;
+using GymManagementDAL.Models.Entities;
+using GymManagementDAL.UnitOfWork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymManagementBll.Services.Classes
+{
+    public class PlanServiec : IPlanServiec
+    {
+        private UnitOfWork _unitOfWork;
+         public PlanServiec(UnitOfWork unitOfWork) {
+          _unitOfWork = unitOfWork;
+        }
+        #region GetAll
+        public IEnumerable<PlanViewModel> GetAllPlans()
+        {
+            var Plans = _unitOfWork.GetRepository<Plan>().GetAll();
+            if (Plans == null || !Plans.Any()) return [];
+            return Plans.Select(p => new PlanViewModel()
+            {
+                id = p.Id,
+                Name = p.Name,
+                Description = p.Description,
+                DurationDays = p.DurationDays,
+                IsActive = p.IsVctive,
+                Price = p.Pirce,
+
+            });
+        }
+        #endregion
+        #region GetByID
+
+        public PlanViewModel? GetPlanById(int id)
+        {
+            var Plan = _unitOfWork.GetRepository<Plan>().GetById(id);
+            if (Plan is null) return null;
+            return new PlanViewModel() { 
+              id = Plan.Id,
+              Name = Plan.Name,
+              Description = Plan.Description,
+              DurationDays = Plan.DurationDays,
+              IsActive = Plan.IsVctive,
+              Price = Plan.Pirce,
+            
+            };
+        }
+        #endregion
+        #region ToggleStatus
+
+        public bool ToggleStatus(int id)
+        {
+            var PlanRepo = _unitOfWork.GetRepository<Plan>();
+            var Plan = PlanRepo.GetById(id);
+            if (Plan is null || Plan.IsVctive == false || HasMemberActive(id)) return false;
+            Plan.IsVctive = Plan.IsVctive == true ? false : true;
+            Plan.UpdateAt = DateTime.Now;
+            try
+            {
+                PlanRepo.Update(Plan);
+                return _unitOfWork.SaveChanges() >0;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+
+        }
+        #endregion
+
+        #region Update View Model
+
+        public UpdatePlanViewModel UpdatePlanById(int  PlanId)
+        {
+            var Plan = _unitOfWork.GetRepository<Plan>().GetById(PlanId);
+            if (Plan is null  || Plan.IsVctive == false || HasMemberActive(PlanId)) return null;
+            return new UpdatePlanViewModel()
+            {
+                Name = Plan.Name,
+                Description = Plan.Description,
+                DurationDays = Plan.DurationDays,
+                Price = Plan.Pirce,
+            };
+
+
+
+        } 
+        public bool UpdatePlan(int id, UpdatePlanViewModel planViewModel)
+        {
+            var Plan = _unitOfWork.GetRepository<Plan>().GetById(id);
+            if(Plan is null || HasMemberActive(id) ) return false;
+            try
+            {
+                Plan.Name = planViewModel.Name;
+                Plan.Description = planViewModel.Description;
+                Plan.DurationDays = planViewModel.DurationDays;
+                Plan.Pirce = planViewModel.Price;
+                Plan.UpdateAt = DateTime.Now;
+                _unitOfWork.GetRepository<Plan>().Update(Plan);
+                return _unitOfWork.SaveChanges() > 0;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+
+        }
+
+
+
+        #endregion
+        #region Helper Function
+       private bool HasMemberActive(int planId)
+        {
+            var ActiveMember = _unitOfWork.GetRepository<MemberShip>().GetAll(ms => ms.PlanId == planId && ms.Status=="Active");
+            return ActiveMember.Any();
+        } 
+
+        #endregion
+
+
+
+
+    }
+}
---
+++ GymManagementBll/ViewModels/PlanViewModels/UpdatePlanViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymManagementBll.ViewModels.PlanViewModels
+{
+    public class UpdatePlanViewModel
+    {
+        #region Name 
+        [Required(ErrorMessage = " You Must Enter Your Name")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "You Must Write Your Name Between 3 and  50 Charaters ")]
+        [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "Name an Contain Only Letters And Spaces ")]
+        public string Name { get; set; } = null!;
+        #endregion
+        #region Description
+        [Required(ErrorMessage = " You Must Enter Your Description")]
+        [StringLength(300, MinimumLength = 10, ErrorMessage = "You Must Write Your Name Between 10 and  300 Charaters ")]
+        public string Description { get; set; } = null!;
+        #endregion
+        #region DurationDays
+        [Required(ErrorMessage = "The Duration Days  is Reqired")]
+
+        [Range(1,365, ErrorMessage = "Duration Days Must Be Between 1 to 365 Days")]
+        public int DurationDays { get; set; }
+
+        #endregion
+        #region Price
+        [Required(ErrorMessage ="The Price Is Required")]
+        [Range( 250,10000, ErrorMessage = "Duration Days Must Be Between 250 to 10000 ")]
+         
+        public decimal Price { get; set; }
+
+        #endregion
+    }
+}
